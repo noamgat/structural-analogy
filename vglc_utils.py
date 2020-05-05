2f905f470f58ef8660b3424d1201c968189c9c16
Noam Gat <noamgat@gmail.com>
2020-05-05 20:17:19 +0300

    Added visualization of population vectors

--- vglc_utils.py
+++ vglc_utils.py
@@ -1,3 +1,4 @@
+import collections
 import math
 import cv2
 import PIL
@@ -5,7 +6,7 @@ import torchvision
 import numpy as np
 from skimage.transform import resize
 from vglc_reader import VGLCLevelRepresentationType, VGLCGameData
-
+from typing import List
 
 def create_downsample_path(in_height, in_width, kernel_size, num_halfings):
     # We want the result to be more or less square. Therefore we will have more X halfings than Y
@@ -56,4 +57,60 @@ def transform_input_vglc(json_path, opt):
                 res.append(scale_channels)
             levels.append(res)
 
+    save_visualizations_on_load = True
+    if save_visualizations_on_load:
+        for levelidx, level in enumerate(levels):
+            fn = f'VisualizeLevels/TestLevel{levelidx}.png'
+            arrs = [visualize_level(game, arr) for arr in level]
+            save_numpy_rgb_uint8_images(arrs, fn)
     return levels
+
+
+def get_tile_color(tile_properties):
+    if 'empty' in tile_properties:
+        return 255, 255, 255
+    if 'hazard' in tile_properties:
+        return 255, 0, 0
+    if 'collectable' in tile_properties:
+        return 255, 255, 0
+    return 150, 75, 0
+
+
+def visualize_level(vglc_game_data: VGLCGameData, population_vector: np.ndarray):
+    per_pixel_output_dimension = 5
+    pixels_per_population = (per_pixel_output_dimension ** 2)
+    stride = per_pixel_output_dimension + 1
+    result = np.zeros((population_vector.shape[0]*stride, population_vector.shape[1]*stride, 3), dtype=np.uint8)
+    indexed_colors = [get_tile_color(vglc_game_data.tile_info[k]) for k in vglc_game_data.sorted_tile_types]
+    for y in range(population_vector.shape[0]):
+        for x in range(population_vector.shape[1]):
+            population = population_vector[y, x]
+            histogram = collections.defaultdict(int)
+            for index, density in enumerate(population):
+                histogram[indexed_colors[index]] += density
+            pixels_placed = 0
+            for color, density in histogram.items():
+                pixels_to_place = int(round(density * pixels_per_population))
+                for _ in range(pixels_to_place):
+                    if pixels_placed < pixels_per_population:
+                        result[y*stride + (pixels_placed // per_pixel_output_dimension), x*stride + (pixels_placed % per_pixel_output_dimension)] = color
+                        pixels_placed += 1
+    return result
+
+
+def save_numpy_rgb_uint8_images(arr: List[np.ndarray], filename: str):
+    from PIL import Image
+    images = [Image.fromarray(a) for a in arr]
+    widths, heights = zip(*(i.size for i in images))
+
+    max_width = max(widths)
+    total_height = sum(heights)
+
+    new_im = Image.new('RGB', (max_width, total_height))
+
+    y_offset = 0
+    for im in images:
+        new_im.paste(im, (0, y_offset))
+        y_offset += im.size[1]
+
+    new_im.save(filename)
